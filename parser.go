dcfae7cbc2a9e9d6f6b4b09f2a83134e5d316d76
Doaa Saber <54359761+doaasaber@users.noreply.github.com>
2022-07-18 16:21:21 +0200

    Update parser.go

--- parser.go
+++ parser.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Checking(result string) bool {
-	regularExpression := regexp.MustCompile("'['[a-z|A-Z]+']")
+	regularExpression := regexp.MustCompile("[[a-z|A-Z]+]")
 	if regularExpression.MatchString(result) {
 		//fmt.Println("result is :", result, " pass")
 		return true
@@ -26,32 +26,76 @@ func Checking(result string) bool {
 }
 
 type Inforamation struct {
-	Data     map[string]map[string]string
-	Comments []string
+	Data map[string]map[string]string
+
+	Comments [10]string
 }
 
 func Search(dictionary map[string]map[string]string, section string, key string) string {
 	return dictionary[section][key]
 }
 
-func LoadFromFile(name string) {
+func LoadFromFile(name string, info Inforamation) {
 	file, ferr := os.Open(name)
 	if ferr != nil {
 		panic(ferr)
 	}
 
 	scanner := bufio.NewScanner(file)
-	LoadFromString(scanner)
+	LoadFromString(scanner, info)
 }
 
-func LoadFromString(scanner *bufio.Scanner) {
+func LoadFromString(scanner *bufio.Scanner, info Inforamation) {
 
+	var section string
+	cont := make(map[string]string)
+
+	m := make(map[string]map[string]string)
+	length := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		items := strings.Split(line, " ")
+		//fmt.Println("number of items:", len(items), items[0])
 
-		fmt.Println(items[0])
+		if items[0] == ";" {
+
+			for i := 0; i < len(items); i++ {
+				info.Comments[length] = line
+			}
+			length++
+
+		} else if Checking(items[0]) {
+			section = items[0]
+
+		} else if len(items) == 1 && items[0] == " " {
+			section = " "
+
+		} else {
+
+			split_equal := strings.Split(line, "=")
+
+			if len(split_equal) == 2 {
+				cont[split_equal[0]] = split_equal[1]
+				m[section] = cont
+				//	info.Data = m
+
+				/*fmt.Println(cont)
+				fmt.Println("before_delete", m)
+
+				delete(cont, split_equal[0])
+				fmt.Println("after delete", cont)
+				fmt.Println("sub_map", m)*/
+			}
+			info.Data = m
+			for k := range cont {
+				delete(cont, k)
+			}
+
+		}
+
 	}
+	//info.Data = m
+	fmt.Println(info.Data)
 
 }
 func check_name(name string) bool {
@@ -111,9 +155,9 @@ func main() {
 			"key1": "val1"
 		},
 	   }*/
-	// var info []Information
+	var info Inforamation
 	var name = "text.INI"
-	LoadFromFile(name)
+	LoadFromFile(name, info)
 
 	/*Data := map[string]map[string]string{
 		"section1": {
@@ -131,7 +175,7 @@ func main() {
 	fmt.Println("return value 2 : ", Search(Data, "section2", "key1"))
 
 	Checking("jj")*/
-	fmt.Println("checking", check_org("Acme Widgets Inc."))
+	//fmt.Println("checking", Checking("[hhk]"))
 
 }
 
