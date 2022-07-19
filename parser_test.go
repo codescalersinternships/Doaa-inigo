7beed9ac3b6121a7692bac5e46810d7a96536acd
Doaa Saber <54359761+doaasaber@users.noreply.github.com>
2022-07-19 17:12:24 +0200

    updated

--- parser_test.go
+++ parser_test.go
@@ -3,8 +3,20 @@ package main
 import (
 	"testing"
 )
-func TestLoadFromFile(t *testing.T) {
-	//parse Parser 
-got := 
+
+func (p *Parser) TestLoadFromFile(t *testing.T) {
+	//got :=
 
 }
+
+/*
+checking section name
+file name
+empty file
+sections without key, values
+more than value (username = doaa =amira )
+get sections
+save file () invalidname
+get value
+search for non existing key , section
+*/
