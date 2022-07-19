80358e09fb2c232bd169d1c0ca39b20ff2eee31f
Doaa Saber <54359761+doaasaber@users.noreply.github.com>
2022-07-19 09:24:51 +0200

    add some functions

---
+++ parser.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+func Checking(result string) bool {
+	regularExpression := regexp.MustCompile("[[a-z|A-Z]+]")
+	if regularExpression.MatchString(result) {
+		return true
+
+	}
+
+	return false
+
+}
+
+type Inforamation struct {
+	Data     map[string]map[string]string
+	Comments [10]string
+}
+
+func GetSectionNames(D map[string]map[string]string) {
+
+	keys := make([]string, 0, len(D))
+	for k := range D {
+		keys = append(keys, k)
+
+	}
+
+	fmt.Println("sections are :", keys)
+}
+
+func Get_value(dictionary map[string]map[string]string, section string, key string) string {
+	return dictionary[section][key]
+}
+
+func LoadFromFile(name string, info Inforamation) {
+	file, ferr := os.Open(name)
+	if ferr != nil {
+		panic(ferr)
+	}
+
+	scanner := bufio.NewScanner(file)
+	LoadFromString(scanner, info)
+}
+
+func LoadFromString(scanner *bufio.Scanner, info Inforamation) {
+	var key string
+	var value string
+	d := make(map[string]map[string]string)
+
+	var section string
+
+	length := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		items := strings.Split(line, " ")
+
+		if items[0] == ";" {
+
+			for i := 0; i < len(items); i++ {
+				info.Comments[length] = line
+			}
+			length++
+
+		} else if Checking(items[0]) {
+			section = items[0]
+			d[section] = make(map[string]string)
+
+		} else if len(items) == 1 && items[0] == " " {
+			section = " "
+
+		} else {
+
+			split_equal := strings.Split(line, "=")
+
+			if len(split_equal) == 2 {
+
+				key = split_equal[0]
+				value = split_equal[1]
+
+				d[section][key] = value
+
+			}
+
+		}
+
+	}
+	info.Data = d
+	fmt.Println(info.Data)
+
+}
+func check_name(name string) bool {
+	regularExpression := regexp.MustCompile("[a-z|A-Z]+")
+	if regularExpression.MatchString(name) {
+		return true
+
+	}
+	return false
+}
+
+//"1K345"
+func check_port(port string) bool {
+	for _, c := range port {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+func check_FileName(name string) bool {
+
+	regularExpression := regexp.MustCompile("[.].[txt|dat]")
+	if regularExpression.MatchString(name) {
+		return true
+
+	}
+	return false
+}
+
+func check_server(server string) bool {
+
+	regularExpression := regexp.MustCompile("[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+")
+	if regularExpression.MatchString(server) {
+		return true
+
+	}
+	return false
+}
+
+func check_org(org string) bool {
+
+	regularExpression := regexp.MustCompile("[a-z|A-Z]+[.]")
+	if regularExpression.MatchString(org) {
+		return true
+
+	}
+	return false
+}
+
+func main() {
+
+	var info Inforamation
+	var name = "text.INI"
+	LoadFromFile(name, info)
+	/*fmt.Println(info.Comments)
+	d := make(map[string]map[string]string)
+	d["database"] = make(map[string]string)
+	d["database"]["username"] = "abc"
+	d["database"]["password"] = "dmdm"
+	d["owner"] = make(map[string]string)
+
+	GetSectionNames(d)
+	fmt.Println(Get_value(d, "database", "username"))*/
+}
